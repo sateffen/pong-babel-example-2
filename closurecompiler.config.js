c316ff4855a551587b65c37a24b2581ee4d65909
Steffen W <sateffen@gmail.com>
2016-08-27 00:09:28 +0200

    Optimized closure compiler configuration

--- closurecompiler.config.js
+++ closurecompiler.config.js
@@ -4,30 +4,27 @@ const JAVA_PATH = 'java';
 const JAR_PATH = require.resolve('google-closure-compiler/compiler.jar');
 // then we load our dependencies, the spawn function and glob
 const spawn = require('child_process').spawn;
-const glob = require('glob');
 // and we setup an array for all arguments we want to pass to the command line
 const args = ['-jar', JAR_PATH];
 
-// then setup some command line flags, that configure the input, optimizations and
-// output
+// first we specify some flags about the compilation process
 args.push('--env', 'BROWSER');
 args.push('--language_in', 'ECMASCRIPT6');
+args.push('--language_out', 'ECMASCRIPT5');
 args.push('--compilation_level', 'ADVANCED_OPTIMIZATIONS');
+args.push('--warning_level', 'VERBOSE');
+
+// then we define how to get all the sourcefiles. With the dependency_mode and entry_point
+// we tell the closure compiler how to build it's sourcecode tree
+args.push('--dependency_mode', 'STRICT');
+args.push('--entry_point', 'src/main.js');
+// and yes, we need to specify this twice, because we need to mention the entry poit
+// specifically, and the glob pattern for everything else
+args.push('--js', 'src/main.js');
+args.push('--js', 'src/**/*.js');
+
+// and finally we define where to write the output
 args.push('--js_output_file', 'dist/closurecompiler.js');
 
-// and now search for all sourcefiles
-glob('src/**/*.js', {}, (aError, aFiles) => {
-    // if an error occured simply rethrow it
-    if (aError) {
-        throw aError;
-    }
-    // else convert each file to a commandline flag for the compiler
-    aFiles.forEach((aFile) => {
-        args.push('--js', aFile);
-    });
-
-    // now we have all commandline flags together, so we can spawn the main
-    // process with all args, and tell it to share the same stdio as this process,
-    // so we know everything that happens.
-    spawn(JAVA_PATH, args, { stdio: 'inherit' });
-});
+// finally execute the java process with shared stdio
+spawn(JAVA_PATH, args, { stdio: 'inherit' });
